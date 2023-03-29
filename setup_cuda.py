0444654447172a1de5095c82d9fc6dca2ac5a5e4
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2023-03-29 00:56:51 -0400

    Fix compilation of faster kernels with HIP

--- setup_cuda.py
+++ setup_cuda.py
@@ -1,10 +1,22 @@
 from setuptools import setup, Extension
 from torch.utils import cpp_extension
 
+# Torch defines __HIP_NO_HALF_CONVERSIONS__ and __HIP_NO_HALF_OPERATORS__ for
+# some reason, preventing implicit casts in the faster kernels. This just
+# undefs them.
+HIP_HALF_CONVERSION_FLAGS = [
+    "-U__HIP_NO_HALF_CONVERSIONS__",
+    "-U__HIP_NO_HALF_OPERATORS__",
+]
+
 setup(
     name='quant_cuda',
     ext_modules=[cpp_extension.CUDAExtension(
-        'quant_cuda', ['quant_cuda.cpp', 'quant_cuda_kernel.cu']
+        name='quant_cuda',
+        sources=['quant_cuda.cpp', 'quant_cuda_kernel.cu'],
+        extra_compile_args={
+            'nvcc': HIP_HALF_CONVERSION_FLAGS,
+        }
     )],
     cmdclass={'build_ext': cpp_extension.BuildExtension}
 )
